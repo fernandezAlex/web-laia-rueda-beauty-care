83bc698fb438a1c9c100db1a843463fad2ffb4cc
FernandezAlex <alex.fernandez.arroyo@gmail.com>
2024-04-04 17:36:17 +0200

    fix: agregar programación defensiva en el formateo de los textos de los detalles de servicio

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,24 +1,27 @@
 export const slugGenerator = (treatmentName) => {
-  const slug = `${treatmentName.replace(/\s+/g, "-").toLowerCase()}`;
-  return slug;
+	if (!treatmentName) return;
+	const slug = `${treatmentName.replace(/\s+/g, "-").toLowerCase()}`;
+	return slug;
 };
 
 export const formatText = (text) => {
-  return text
-    .replace(/\s+\\/g, "\\")
-    .replace(/\s*\?/g, "")
-    .replace(/(\\r\\n)(?=\w*\?)/g, "")
-    .replace(/\\r\\n/g, "<br>");
-  s;
+	if (!text) return;
+	return text
+		.replace(/\s+\\/g, "\\")
+		.replace(/\s*\?/g, "")
+		.replace(/(\\r\\n)(?=\w*\?)/g, "")
+		.replace(/\\r\\n/g, "<br>");
+	s;
 };
 
 export const faqsGenerator = (preguntas) => {
-  const faqs = {};
-  preguntas.split("¿").forEach((pregunta) => {
-    const [key, value] = pregunta.split("?");
-    if (key && !faqs[key]) {
-      faqs[key] = formatText(value);
-    }
-  });
-  return Object.entries(faqs);
+	if (!preguntas) return;
+	const faqs = {};
+	preguntas.split("¿").forEach((pregunta) => {
+		const [key, value] = pregunta.split("?");
+		if (key && !faqs[key]) {
+			faqs[key] = formatText(value);
+		}
+	});
+	return Object.entries(faqs);
 };
