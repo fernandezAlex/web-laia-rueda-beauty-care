9aca0abf71e8101a465e5280f7950ab0c485d0a6
FernandezAlex <alex.fernandez.arroyo@gmail.com>
2024-09-08 18:51:45 +0200

    añadir DATABASE_URL como vairbale de env para coenctar con base de datos nueva de produccion

--- config/db.js
+++ config/db.js
@@ -1,25 +1,37 @@
 import Sequelize from "sequelize";
-import dotenv from "dotenv/config";
+import dotenv from "dotenv";
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    define: {
-      timestamps: false,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    operatorAliases: false,
-  }
-);
+// Cargar las variables de entorno
+dotenv.config();
+
+// Usa DATABASE_URL si está definida
+const dbConfig = process.env.DATABASE_URL || {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	dialect: "mysql",
+	define: {
+		timestamps: false,
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+	operatorAliases: false,
+};
+
+const db = new Sequelize(dbConfig);
+
+db.authenticate()
+	.then(() => {
+		console.log("Conexión a la base de datos exitosa");
+	})
+	.catch((error) => {
+		console.error("Error al conectar a la base de datos:", error);
+	});
 
 export default db;
